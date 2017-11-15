60e4ca3e765be05edfe13ed64b940578ada6e674
jungwoon <jwmax2002>
2017-11-15 15:04:12 +0900

    Success

--- src/main/java/com/byjw/dataflow/TwitterPipeline.java
+++ src/main/java/com/byjw/dataflow/TwitterPipeline.java
@@ -10,11 +10,13 @@ import com.byjw.nl.NLAnalyze;
 import com.byjw.nl.NLAnalyzeVO;
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions;
 import com.google.cloud.dataflow.sdk.runners.BlockingDataflowPipelineRunner;
+import com.google.cloud.dataflow.sdk.runners.DataflowPipelineRunner;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,14 +36,10 @@ import com.google.cloud.dataflow.sdk.transforms.windowing.Window;
 import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-
 public class TwitterPipeline {
     private static final Logger LOG = LoggerFactory.getLogger(TwitterPipeline.class);
     private static final String PROJECT = "beer-coding";
-    private static final String NOWN_TABLE = "beer-coding:twitter.noun";
+    private static final String NOUN_TABLE = "beer-coding:twitter.noun";
     private static final String ADJ_TABLE = "beer-coding:twitter.adj";
     private static final String TOPIC = "projects/beer-coding/topics/twitter";
     private static final String BUCKET = "gs://dataflow-byjw";
@@ -50,16 +48,15 @@ public class TwitterPipeline {
 
     public static void main(String[] args) {
 
-        DataflowPipelineOptions options = PipelineOptionsFactory.create().as(DataflowPipelineOptions.class);
-        options.setRunner(BlockingDataflowPipelineRunner.class);
-        options.setProject(PROJECT);
-        options.setStagingLocation(BUCKET);
+        // No args settings
+//        DataflowPipelineOptions options = PipelineOptionsFactory.create().as(DataflowPipelineOptions.class);
+//        options.setRunner(DataflowPipelineRunner.class);
+//        options.setProject(PROJECT);
+//        options.setStagingLocation(BUCKET);
+//        options.setStreaming(true);
+//        Pipeline pipeline = Pipeline.create(options);
 
-        Pipeline pipeline = Pipeline.create(options);
-
-        // Origin
-//        Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.fromArgs(args).withValidation().create());
-
+        Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.fromArgs(args).withValidation().create());
 
         // pipeline.apply를 통해서 변형된 데이터를 PCollection에 담음)
         PCollection <KV<String,Iterable<String>>> nlProcessed =
@@ -68,23 +65,19 @@ public class TwitterPipeline {
                         .apply(ParDo.named("NL Processing").of(new NLAnalyticsDoFn()));
 
         // Noun handling sub-pipeline
-        TableSchema nounSchema = new TableSchema().setFields(getNounFields());
-
         nlProcessed.apply(ParDo.named("NounFilter").of(new NounFilter()))
                 .apply(Window.<String>into(FixedWindows.of(Duration.standardSeconds(30))))
                 .apply(Count.<String>perElement())
                 .apply(ParDo.named("Noun Formating").of(new AddTimeStampNoun()) )
                 .apply(BigQueryIO.Write
                         .named("Write Noun Count to BQ")
-                        .to(NOWN_TABLE)
-                        .withSchema(nounSchema)
+                        .to(NOUN_TABLE)
+                        .withSchema(getNounSchema())
                         .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
                         .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
 
 
         // Adj handling sub-pipeline
-        TableSchema adjSchema = new TableSchema().setFields(getAdjFields());
-
         nlProcessed.apply(ParDo.named("AdjFilter").of(new AdjFilter()))
                 .apply(Window.<String>into(FixedWindows.of(Duration.standardSeconds(30))))
                 .apply(Count.<String>perElement())
@@ -92,34 +85,34 @@ public class TwitterPipeline {
                 .apply(BigQueryIO.Write
                         .named("Write Adj Count to BQ")
                         .to(ADJ_TABLE)
-                        .withSchema(adjSchema)
+                        .withSchema(getAdjSchema())
                         .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
                         .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
 
         pipeline.run();
     }
 
-    private static List<TableFieldSchema> getAdjFields() {
+    private static TableSchema getAdjSchema() {
         List<TableFieldSchema> fields = new ArrayList<>();
         fields.add(new TableFieldSchema().setName("date").setType("TIMESTAMP"));
         fields.add(new TableFieldSchema().setName("adj").setType("STRING"));
         fields.add(new TableFieldSchema().setName("count").setType("INTEGER"));
-        return fields;
+
+        return new TableSchema().setFields(fields);
     }
 
-    private static List<TableFieldSchema> getNounFields() {
+    private static TableSchema getNounSchema() {
         List<TableFieldSchema> fields = new ArrayList<>();
         fields.add(new TableFieldSchema().setName("date").setType("TIMESTAMP"));
         fields.add(new TableFieldSchema().setName("noun").setType("STRING"));
         fields.add(new TableFieldSchema().setName("count").setType("INTEGER"));
 
-        return fields;
+        return new TableSchema().setFields(fields);
     }
 
     // Read Twitter feed as a JSON format
     // extract twitt feed string and pass into next pipeline
-    static class ParseTwitterFeedDoFn extends DoFn<String,String>{
-        private static final long serialVersionUID = 3644510088969272245L;
+    static class ParseTwitterFeedDoFn extends DoFn<String,String> {
 
         @Override
         public void processElement(ProcessContext processContext){
@@ -127,11 +120,10 @@ public class TwitterPipeline {
             String lang;
 
             try {
-                JsonReader jsonReader = Json.createReader(new StringReader(processContext.element()));
-                JsonObject readObject = jsonReader.readObject();
+                JSONObject jsonObject = new JSONObject(processContext.element());
 
-                text = readObject.getString("text");
-                lang = readObject.getString("lang");
+                text = jsonObject.getString("text");
+                lang = jsonObject.getString("lang");
 
                 if (lang.equals("en")) {
                     processContext.output(text.toLowerCase());
@@ -151,15 +143,12 @@ public class TwitterPipeline {
     // - list of emoticon
 
     static class NLAnalyticsDoFn extends DoFn< String, KV<String, Iterable<String>> > {
-        private static final long serialVersionUID = 3013780586389810713L;
 
-        // return list of NOUN,ADJ,Emoticon
+        // return list of NOUN, ADJ, Emoticon
         @Override
         public void processElement(ProcessContext processContext) throws IOException, GeneralSecurityException {
             String text = processContext.element();
-
-            NLAnalyze nlAnalyze = NLAnalyze.getInstance();
-            NLAnalyzeVO nlAnalyzeVO = nlAnalyze.analyze(text);
+            NLAnalyzeVO nlAnalyzeVO = NLAnalyze.getInstance().analyze(text);
 
             List<String> nouns = nlAnalyzeVO.getNouns();
             List<String> adjs = nlAnalyzeVO.getAdjs();
@@ -194,21 +183,24 @@ public class TwitterPipeline {
             implements com.google.cloud.dataflow.sdk.transforms.DoFn.RequiresWindowAccess
     {
         @Override
-        public void processElement(ProcessContext c) {
-            String key = c.element().getKey();	// get Word
-            Long value = c.element().getValue();// get count of the word
-            IntervalWindow w = (IntervalWindow) c.window();
-            Instant s = w.start();
-            DateTime sTime = s.toDateTime(org.joda.time.DateTimeZone.forID("Asia/Seoul"));
-            DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
-            String str_stime = sTime.toString(dtf);
+        public void processElement(ProcessContext processContext) {
+            String key = processContext.element().getKey();	// get Word
+            Long value = processContext.element().getValue();// get count of the word
+
+            IntervalWindow intervalWindow = (IntervalWindow) processContext.window();
+            Instant start = intervalWindow.start();
+
+            DateTime startTime = start.toDateTime(org.joda.time.DateTimeZone.forID("Asia/Seoul"));
+            DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+            String strStartTime = startTime.toString(dateTimeFormatter);
 
             TableRow row =  new TableRow()
-                    .set("date", str_stime)
+                    .set("date", strStartTime)
                     .set("noun", key)
                     .set("count", value);
 
-            c.output(row);
+            processContext.output(row);
         }
 
     }
@@ -216,45 +208,48 @@ public class TwitterPipeline {
     static class AddTimeStampAdj extends DoFn<KV<String,Long>,TableRow>
             implements com.google.cloud.dataflow.sdk.transforms.DoFn.RequiresWindowAccess {
         @Override
-        public void processElement(ProcessContext c) {
-            String key = c.element().getKey();	// get Word
-            Long value = c.element().getValue();// get count of the word
-            IntervalWindow w = (IntervalWindow) c.window();
-            Instant s = w.start();
-            DateTime sTime = s.toDateTime(org.joda.time.DateTimeZone.forID("Asia/Seoul"));
+        public void processElement(ProcessContext processContext) {
+            String key = processContext.element().getKey();	// get Word
+            Long value = processContext.element().getValue();// get count of the word
+
+            IntervalWindow intervalWindow = (IntervalWindow) processContext.window();
+            Instant start = intervalWindow.start();
+
+            DateTime startTime = start.toDateTime(org.joda.time.DateTimeZone.forID("Asia/Seoul"));
             DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
-            String str_stime = sTime.toString(dtf);
+
+            String strStartTime = startTime.toString(dtf);
 
             TableRow row =  new TableRow()
-                    .set("date", str_stime)
+                    .set("date", strStartTime)
                     .set("adj", key)
                     .set("count", value);
 
-            c.output(row);
+            processContext.output(row);
         }
 
     }
+
     static class AdjFilter extends DoFn<KV<String,Iterable<String>>,String> {
         @Override
-        public void processElement(ProcessContext c) {
-            String key = c.element().getKey();
+        public void processElement(ProcessContext processContext) {
+            String key = processContext.element().getKey();
             if(!key.equals("ADJ")) return;
-            List<String> values = (List<String>) c.element().getValue();
+            List<String> values = (List<String>) processContext.element().getValue();
             for(String value:values){
-                c.output(value);
+                processContext.output(value);
             }
         }
     }
 
     static class Echo extends DoFn<KV<String,Iterable<String>>,Void> {
         @Override
-        public void processElement(ProcessContext c) {
-            String key = c.element().getKey();
-            List<String> values = (List<String>) c.element().getValue();
+        public void processElement(ProcessContext processContext) {
+            String key = processContext.element().getKey();
+            List<String> values = (List<String>) processContext.element().getValue();
             for (String value:values){ }
         }
 
     }
-
 
 }
---
+++ src/test/java/com/example/NLAnalyzeTest.java
@@ -0,0 +1,75 @@
+package com.example;
+
+
+import com.byjw.nl.NLAnalyze;
+import com.byjw.nl.NLAnalyzeVO;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+
+import static org.junit.Assert.fail;
+
+public class NLAnalyzeTest {
+
+    @Test
+    public void nl_test() {
+
+        try {
+            NLAnalyze instance = NLAnalyze.getInstance();
+            String text="inspects the given text for known entities (proper nouns and common nouns), returns information about those entities, and identifies the prevailing emotional opinion of the entity within the text, especially to determine a writer's attitude toward the entity as positive, negative, or neutral. Entity analysis is performed with the analyzeEntitySentiment method.";
+
+            NLAnalyzeVO vo = instance.analyze(text);
+
+            List<String> nouns = vo.getNouns();
+            List<String> adjs = vo.getAdjs();
+
+            System.out.println("### NOUNS");
+
+            for(String noun:nouns){
+                System.out.println(noun);
+            }
+
+
+            System.out.println("\n### ADJS");
+
+            for(String adj:adjs){
+                System.out.println(adj);
+            }
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            fail("API call error");
+        }
+        catch (GeneralSecurityException e) {
+            e.printStackTrace();
+            fail("Security exception");
+        }
+    }
+
+    @Test
+    public void parse_json() {
+
+        String Sample = "{\"created_at\":\"Wed Nov 15 05:03:18 +0000 2017\",\"id\":930662470758420482,\"id_str\":\"930662470758420482\",\"text\":\"RT @Apple_believer_: 「✨iphone X✨」\\n\\n速報！iphoneX入荷しました！！\\n\\n気になる応募方法はRTorFollowのみ✨\\n\\nさらに今までに当選経験がない方には当選確率UP\uD83D\uDD3A\uD83D\uDD3A\\n\\nフォロワーが少ないうちは当選確率がさらに上がります(^^)… \",\"source\":\"<a href=\\\"http://twitter.com/download/iphone\\\" rel=\\\"nofollow\\\">Twitter for iPhone</a>\",\"truncated\":false,\"in_reply_to_status_id\":null,\"in_reply_to_status_id_str\":null,\"in_reply_to_user_id\":null,\"in_reply_to_user_id_str\":null,\"in_reply_to_screen_name\":null,\"user\":{\"id\":1030859467,\"id_str\":\"1030859467\",\"name\":\"リノ\",\"screen_name\":\"smoker_mb\",\"location\":\"奈良 近畿地方\",\"url\":null,\"description\":\"ぽっちゃり系が大好き(*´ω｀*)\",\"translator_type\":\"none\",\"protected\":false,\"verified\":false,\"followers_count\":59,\"friends_count\":676,\"listed_count\":0,\"favourites_count\":739,\"statuses_count\":42,\"created_at\":\"Sun Dec 23 16:07:56 +0000 2012\",\"utc_offset\":null,\"time_zone\":null,\"geo_enabled\":false,\"lang\":\"ja\",\"contributors_enabled\":false,\"is_translator\":false,\"profile_background_color\":\"C0DEED\",\"profile_background_image_url\":\"http://abs.twimg.com/images/themes/theme1/bg.png\",\"profile_background_image_url_https\":\"https://abs.twimg.com/images/themes/theme1/bg.png\",\"profile_background_tile\":false,\"profile_link_color\":\"1DA1F2\",\"profile_sidebar_border_color\":\"C0DEED\",\"profile_sidebar_fill_color\":\"DDEEF6\",\"profile_text_color\":\"333333\",\"profile_use_background_image\":true,\"profile_image_url\":\"http://pbs.twimg.com/profile_images/764024410621698048/lLMKhGVf_normal.jpg\",\"profile_image_url_https\":\"https://pbs.twimg.com/profile_images/764024410621698048/lLMKhGVf_normal.jpg\",\"default_profile\":true,\"default_profile_image\":false,\"following\":null,\"follow_request_sent\":null,\"notifications\":null}}";
+        String text;
+
+        try {
+
+            JSONObject obj = new JSONObject(Sample);
+            text = obj.getString("text");
+
+            System.out.print("\nJSON Parsing : " + text);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            fail("No text element");
+            fail("original message is :");
+        }
+    }
+
+
+}
